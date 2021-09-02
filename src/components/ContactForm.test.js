b519ab0bf352727ea0044a2b6f007e2f8d359ad8
jMisch44 <jennimisch@gmail.com>
2021-09-02 17:37:50 -0400

    working on second to last test

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -15,39 +15,89 @@ test("renders the contact form header", () => {
 });
 
 test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
-  //Arrange
   render(<ContactForm />);
-  //Act
-  // focus on firstname input field
+
   const firstNameInput = screen.getByLabelText(/First Name*/i);
-  //type less than 5 characters
   userEvent.type(firstNameInput, "Hi");
-  //Assert
-  //check that error is on screen
+
   const firstNameValidation = screen.getByText(
     /Error: firstName must have at least 5 characters./i
   );
   expect(firstNameValidation).toBeInTheDocument();
 });
 
-test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  render(<ContactForm />);
+
+  const submitFormButton = screen.getByRole("button");
+  userEvent.click(submitFormButton);
+
+  const firstNameValidation = screen.getByText(
+    /Error: firstName must have at least 5 characters./i
+  );
+  expect(firstNameValidation).toBeInTheDocument();
+  const lastNameValidation = screen.getByText(
+    /Error: lastName is a required field./i
+  );
+  expect(lastNameValidation).toBeInTheDocument();
+  const emailValidation = screen.getByText(
+    /Error: email must be a valid email address./i
+  );
+  expect(emailValidation).toBeInTheDocument();
 });
 
-test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
+  render(<ContactForm />);
+
+  const firstNameInput = screen.getByLabelText(/First Name*/i);
+  userEvent.type(firstNameInput, "Qwerty");
+  const lastNameInput = screen.getByLabelText(/Last Name*/i);
+  userEvent.type(lastNameInput, "Please");
+  const emailInput = screen.getByLabelText(/email*/i);
+  userEvent.type(emailInput, "j");
+
+  const emailValidation = screen.getByText(
+    /Error: email must be a valid email address./i
+  );
+  expect(emailValidation).toBeInTheDocument();
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+  render(<ContactForm />);
+  const emailInput = screen.getByLabelText(/email*/i);
+  userEvent.type(emailInput, "j");
+
+  const emailValidation = screen.getByText(
+    /Error: email must be a valid email address./i
+  );
+  expect(emailValidation).toBeInTheDocument();
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+  render(<ContactForm />);
+
+  const firstNameInput = screen.getByLabelText(/First Name*/i);
+  userEvent.type(firstNameInput, "Qwerty");
+  const emailInput = screen.getByLabelText(/email*/i);
+  userEvent.type(emailInput, "email@email.com");
+  const submitFormButton = screen.getByRole("button");
+  userEvent.click(submitFormButton);
 });
 
-test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
+test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {
+  render(<ContactForm />);
+
+  const firstNameInput = screen.getByLabelText(/First Name*/i);
+  userEvent.type(firstNameInput, "Qwerty");
+  const lastNameInput = screen.getByLabelText(/Last Name*/i);
+  userEvent.type(lastNameInput, "Please");
+  const emailInput = screen.getByLabelText(/email*/i);
+  userEvent.type(emailInput, "email@email.com");
+  const submitFormButton = screen.getByRole("button");
+  userEvent.click(submitFormButton);
+
+  const submittedForm = screen.getByText(/You submitted:/i);
+  expect(submittedForm).toBeInTheDocument();
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
